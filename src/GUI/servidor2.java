5966c0ba31a44b5bade88df93de9c74ae23644af
alexdlveliz <alexdlveliz@hotmail.com>
2019-04-30 21:02:48 -0600

    agregadas clases de prueba

--- src/GUI/servidor2.java
+++ src/GUI/servidor2.java
@@ -5,9 +5,11 @@
  */
 package GUI;
 
+import clases.paquete_de_envio;
 import java.awt.BorderLayout;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
@@ -48,11 +50,23 @@ class marcoServidor extends JFrame implements Runnable
     public void run() {
         try {
             ServerSocket  servidor = new ServerSocket(9999);
-            Socket misocket = servidor.accept();
-            DataInputStream flujo_entrada = new DataInputStream(misocket.getInputStream());
-            String mensaje_texto = flujo_entrada.readUTF();
-            
-        } catch (IOException ex) {
+            String nick, ip, mensaje;
+            paquete_de_envio paquete_recibido;
+            while(true)
+            {
+                Socket misocket = servidor.accept();
+                ObjectInputStream paquete_datos = new ObjectInputStream(misocket.getInputStream());
+                paquete_recibido = (paquete_de_envio) paquete_datos.readObject();
+                nick = paquete_recibido.getNick();
+                ip = paquete_recibido.getIp();
+                mensaje = paquete_recibido.getMensaje();
+                /*DataInputStream flujo_entrada = new DataInputStream(misocket.getInputStream());
+                String mensaje_texto = flujo_entrada.readUTF();
+                areatexto.append("\n"+mensaje_texto);*/
+                areatexto.append("\n" + nick + ":" + mensaje + " para "+ ip);
+                misocket.close();
+            }
+        } catch (IOException | ClassNotFoundException ex) {
             Logger.getLogger(marcoServidor.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
